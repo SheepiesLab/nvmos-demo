c3d5d5e5f6fdef8735a6789dda8e121650d585a3
Ben NG Hok Chun <hcngac@localhost.localdomain>
2019-04-12 17:32:44 +0800

    demo test

--- demo.c
+++ demo.c
@@ -17,44 +17,50 @@ void _start()
 
 	printf("Hello from userspace!\n");
 
+	char input[0x1000];
+	const char *str = "asdfghjk";
 	int hellofd;
 	void *mapaddr;
+	int file_pages = 0x80;
 	printf(
 		"FD of new file hello.txt: %d\n",
 		hellofd = open("hello.txt", 0));
 	printf(
 		"Result of ftruncate: %d\n",
-		ftruncate(hellofd, 0x1000));
+		ftruncate(hellofd, 0x1000 * file_pages));
 	printf(
 		"Result of mmap: 0x%p\n",
-		(uint64_t)(mapaddr = mmap(0, 0x1000, 0, 0, hellofd, 0)));
-
-	memcpy(mapaddr + 1384, "Hello! I'm Ben! Demoing demoing", 35);
-	lseek(hellofd, 1384, 0);
-	char buf[35];
-	read(hellofd, buf, 35);
-	printf("Test serial reading mmap write: %s\n", buf);
-	lseek(hellofd, 500, 0);
-	write(hellofd, "TEST WRITE TEST WRITE!", 25);
-	printf("Test mmap reading serial write: %s\n", (char *)(mapaddr + 500));
-
-	int hellofd2;
-	void *mapaddr2;
-	printf(
-		"FD2 of file hello.txt: %d\n",
-		hellofd2 = open("hello.txt", 0));
-	lseek(hellofd2, 500, 0);
-	read(hellofd2, buf, 35);
-	printf("Test opened file: %s\n", buf);
-	lseek(hellofd2, 1384, 0);
-	read(hellofd2, buf, 35);
-	printf("Test opened file: %s\n", buf);
-
-	void *mallocaddrs[50];
-	for (i = 1; i < 50; ++i)
-	{
-		printf("Test malloc %d bytes: %p\n", i, (uint64_t)(mallocaddrs[i] = malloc(i)));
+		(uint64_t)(mapaddr = mmap(0, 0x1000 * file_pages, 0, 0, hellofd, 0)));
+	for (i = 0; i < 0x1000; ++i){
+		input[i] = str[i%8];
 	}
+	lseek(hellofd, 0, 0);
+	for (i = 0; i < file_pages; ++i)
+		write(hellofd, input, 0x1000);
+
+	printf("Starting the test in ");
+
+	for (i = 5; i >= 0; --i){
+		for (a = 0; a < 0x20000000; ++a);
+		printf("%d ", i);
+	}
+	printf("\n");
+
+	for (a = 0; a < 0x1000; ++a){
+		lseek(hellofd, 0, 0);
+		for (i = 0; i < file_pages; ++i)
+			read(hellofd, input, 0x1000);
+	}
+
+
+	/*for (a = 0; a < 0x400000; ++a){
+		mapaddr = mmap(0, 0x1000 * file_pages, 0, 0, hellofd, 0);
+		munmap(mapaddr, 0x1000 * file_pages);
+	}*/
+
+	printf("Finished!\n");
+
+	while(1);
 
 	exit();
 }
