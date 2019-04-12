ad43ef1eb5d6626c7fc51110312a3faaae549bda
Ben NG Hok Chun <hcngac@localhost.localdomain>
2019-04-12 17:27:08 +0800

    Optimized memcpy

--- lib/string/memcpy.c
+++ lib/string/memcpy.c
@@ -1,6 +1,21 @@
 #include <string.h>
 
 void *memcpy(void *restrict dstptr, const void *restrict srcptr, size_t size) {
+	int i;
+
+	if (
+			(unsigned)dstptr % sizeof(long) == 0 &&
+			(unsigned)srcptr % sizeof(long) == 0 &&
+			size % sizeof(long) == 0
+			){
+		long *d = dstptr;
+		const long *s = srcptr;
+		for (i = 0; i < size/sizeof(long); ++i){
+			d[i] = s[i];
+		}
+		return dstptr;
+	}
+
     unsigned char *dst = (unsigned char *) dstptr;
     const unsigned char *src = (const unsigned char *) srcptr;
     for (size_t i = 0; i < size; i++)
